7ee43f8a0bc90d1e830769bfd9b886d2fea1438a
Aerotune <bue@Bues-MacBook-Air-2.local>
2013-10-07 20:22:45 +0200

    Fix for putting files in subdirectory

--- cloudy_ui.rb
+++ cloudy_ui.rb
@@ -2,7 +2,7 @@ module CloudyUi
   require_relative 'cloudy_ui/button'
   require_relative 'cloudy_ui/slider'
   
-  def self.load_images window, load_path=File.join(Dir.pwd, 'cloudy_ui/images')
+  def self.load_images window, load_path=File.join(File.dirname(__FILE__), 'cloudy_ui/images')
     CloudyUi::Button.load_images window, load_path
     CloudyUi::Slider.load_images window, load_path
   end
