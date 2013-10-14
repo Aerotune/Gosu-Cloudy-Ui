a3cb0bbc65e1e805a435e8f24380e24b60670af0
Aerotune <bue@Bues-MacBook-Air-2.local>
2013-10-14 22:16:02 +0200

    changed window to match artists original background color

--- cloudy_ui/window.rb
+++ cloudy_ui/window.rb
@@ -1,9 +1,9 @@
 class CloudyUi::Window
   def self.draw window, x, y, z, width, height
-    window.draw_quad x       ,y        ,0xFFFFFFFF,
-                     x+width ,y        ,0xFFFFFFFF,
-                     x+width ,y+height ,0xFFf5f5f5,
-                     x       ,y+height ,0xFFf5f5f5, z
+    window.draw_quad x       ,y        ,0xFFFAFAFA,
+                     x+width ,y        ,0xFFFAFAFA,
+                     x+width ,y+height ,0xFFf3f3f3,
+                     x       ,y+height ,0xFFf3f3f3, z
                      
     window.draw_line x, y       , 0xFFe4e4e4, x+width   , y      , 0xFFe4e4e4   ,z
     window.draw_line x, y+height, 0xFFCCCCCC, x+width   , y+height+1, 0xFFCCCCCC  ,z
