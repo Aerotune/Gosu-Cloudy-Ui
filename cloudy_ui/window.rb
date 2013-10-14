3a40583b974cacbfe57aa07fe17e0fb69bc185b0
Aerotune <bue@Bues-MacBook-Air-2.local>
2013-10-14 21:59:16 +0200

    changed window to match artists original background color

--- cloudy_ui/window.rb
+++ cloudy_ui/window.rb
@@ -1,15 +1,14 @@
 class CloudyUi::Window
   def self.draw window, x, y, z, width, height
-    window.draw_quad x       ,y        ,0xFFf4f4f4,
-                     x+width ,y        ,0xFFf4f4f4,
-                     x+width ,y+height ,0xFFe3e3e3,
-                     x       ,y+height ,0xFFe3e3e3, z
+    window.draw_quad x       ,y        ,0xFFFFFFFF,
+                     x+width ,y        ,0xFFFFFFFF,
+                     x+width ,y+height ,0xFFf5f5f5,
+                     x       ,y+height ,0xFFf5f5f5, z
                      
-    window.draw_line x, y-1       , 0xFFdbdbdb, x+width   , y-1      , 0xFFdbdbdb   ,z
-    window.draw_line x, y         , 0xFFFFFFFF, x+width   , y        , 0xFFFFFFFF   ,z
-    window.draw_line x, y+height+1, 0xFF989898, x+width   , y+height+1, 0xFF989898  ,z
+    window.draw_line x, y       , 0xFFe4e4e4, x+width   , y      , 0xFFe4e4e4   ,z
+    window.draw_line x, y+height, 0xFFCCCCCC, x+width   , y+height+1, 0xFFCCCCCC  ,z
     
-    window.draw_line x        ,y-1,0xFFdbdbdb,x        ,y+height,0xFF989898,z
-    window.draw_line x+width+1,y-1,0xFFdbdbdb,x+width+1,y+height,0xFF989898,z
+    window.draw_line x        ,y,0xFFe4e4e4,x        ,y+height,0xFFCCCCCC,z
+    window.draw_line x+width+1,y,0xFFe4e4e4,x+width+1,y+height,0xFFCCCCCC,z
   end
 end
